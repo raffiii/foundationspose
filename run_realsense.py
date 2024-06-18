616ccd28d09bf9ca70ce59c509bb523420fe5c50
raffiii <uounb@student.kit.edu>
2024-06-18 15:54:34 +0200

    Add foundationspose

--- run_realsense.py
+++ run_realsense.py
@@ -1,11 +1,12 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import trimesh
 
 from estimater import *
 
 
-def run_pose_estimation_worker(color, depth,K, est:FoundationPose=None, debug=0, ob_id=None, device='cuda:0'):
+def run_pose_estimation_worker(color, depth,K, est:FoundationPose, ob_mask=None, debug=0, ob_id=None, device='cuda:0'):
   torch.cuda.set_device(device)
   est.to_device(device)
   est.glctx = dr.RasterizeCudaContext(device=device)
@@ -16,17 +17,25 @@ def run_pose_estimation_worker(color, depth,K, est:FoundationPose=None, debug=0,
 
   debug_dir =est.debug_dir
 
-  pose = est.register(K=K, rgb=color, depth=depth, ob_mask=ob_mask, ob_id=ob_id)
-  logging.info(f"pose:\n{pose}")
-
-  if debug>=3:
-      m = est.mesh_ori.copy()
-      tmp = m.copy()
-      tmp.apply_transform(pose)
-      tmp.export(f'{debug_dir}/model_tf.obj')
-
+  if ob_mask is None:
+    pose = est.register(K=K, rgb=color, depth=depth, ob_mask=ob_mask)
+    logging.info(f"pose:\n{pose}")
+  else:
+    pose = est.track_one(rgb=color, depth=depth, K=reader.K, iteration=args.track_refine_iter)
 
   return pose
+
+
+def build_estimator(mesh):
+
+  to_origin, extents = trimesh.bounds.oriented_bounds(mesh)
+  bbox = np.stack([-extents/2, extents/2], axis=0).reshape(2,3)
+
+  scorer = ScorePredictor()
+  refiner = PoseRefinePredictor()
+  glctx = dr.RasterizeCudaContext()
+  est = FoundationPose(model_pts=mesh.vertices, model_normals=mesh.vertex_normals, mesh=mesh, scorer=scorer, refiner=refiner, debug_dir=debug_dir, debug=debug, glctx=glctx)
+  logging.info("estimator initialization done")
 
 
 def run_with_pipeline(function):
@@ -82,7 +91,26 @@ def run_demo(pipeline):
 
 
 # Precompute the model with run_ycbv/run_linemode in run_nerf.py
-def run_live_estimation(pipeline):
+def run_live_estimation(mesh):
+    est = build_estimator(mesh)
+    def run(pipeline):
+        K = pipeline.todo
+        mask = True
+        while(True):
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+
+            if not depth_frame or not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+            mask = np.ones(color_frame.shape()[:2])
+            run_pose_estimation_worker(color_frame, depth_frame, K, est,ob_mask=mask)        
+        
+        
     pass    
 if __name__ == '__main__':
     run_with_pipeline(run_demo)
