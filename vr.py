2c7daa1d80ec91d1b05cfc8d1836adaa5a225ced
Rafael B <45318499+raffiii@users.noreply.github.com>
2024-08-27 17:53:03 +0200

    Use SimPublisher to communicate with VR

--- vr.py
+++ vr.py
@@ -1,35 +1,51 @@
-import cv2
+import json, random
 import numpy as np
-#import hololabeling.video2label
-from hololabeling.drifter import LabelingServer
-import queue
+from simpub.core.net_manager import init_net_manager, logger
+from simpub.xr_device.xr_device import XRDevice
+from functools import partial
 
-class LabelQueue:
-    def __init__(self):
-        self.queue = queue.Queue(maxsize=10)
+def send_point_cloud(self, point_data) -> None:
+    self.request_socket.send_string(json.dumps(self.point_cloud_data))
 
-    def create_dir(self, _):
+
+def generate_point_cloud_data(rgb_image, depth_image):
+    positions = []
+    colors = []
+    height, width, _ = rgb_image.shape
+    for y in range(height):
+        for x in range(width):
+            rgb_pixel = rgb_image[y, x]
+            depth_pixel = depth_image[y, x]
+            if random.random() > 0.1:
+                continue
+            positions.extend([-float(x / width), float(y / height), -float(depth_pixel)])
+            colors.extend([int(rgb_pixel[0]) / 255, int(rgb_pixel[1]) / 255, int(rgb_pixel[2]) / 255, 1])
+    return json.dumps({"positions": positions, "colors": colors})
+
+def get_bbox(msg, color = None, depth = None, folder = "debug", file = None):
+    o = json.loads(msg)
+    center_pose = np.float64(o["center_pose"])
+    bbox = np.float64(o["bbox"])
+    if file is not None:
+        np.savez(f"{folder}/labeled/{file}", color = color, depth = depth, bbox = bbox, center_pose = center_pose)
+
+
+
+def send_saved_point_cloud(folder,file):
+    path = f"{folder}/{file}"
+    data = np.load(path)
+    color = data['depth'][...,::-1]
+    depth=data['color']
+    send_data = generate_point_cloud_data(color,depth)
+    net_manager = init_net_manager("192.168.0.134")
+    unity_editor = XRDevice("ALRMetaQuest3")
+    unity_editor.register_topic_callback("bbox_submission", partial(get_bbox,color = color, depth = depth, folder = folder, file = file))
+    # print(generate_point_cloud_data(rgb_image, depth_image))
+    while unity_editor.connected is False:
         pass
-
-    def save_file(self, filename, content):
-        self.queue.put((filename, content))
-
-def run_with_labeling_server(f):
-    labelqueue = LabelQueue()
-    server = LabelingServer(
-        file_saver=labelqueue,
-        host="0.0.0.0", 
-        port=8053
-    )
-    try:
-        server.start_server_thread(block=False)
-        f(labelqueue.queue)
-    finally:
-        server.close_server()
+    unity_editor.request("LoadPointCloud", send_data)
+    net_manager.join()
 
 if __name__ == "__main__":
-    def print_all(q):
-        while True:
-            if q.not_empty:
-                print(q.get())
-    run_with_labeling_server(print_all)
+
+    send_saved_point_cloud("debug/frames","1724146523748.npz")
